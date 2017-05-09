a20fbf5495e423025fb61311f03823f8e7db9744
周正飞 <Zafir@zhouzhengfeideMacBook-Pro.local>
2017-05-09 15:37:30 +0800

    用storyboard搭建框架

---
+++ ZFLiveStreaming/Classes/Other/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  ZFLiveStreaming
+//
+//  Created by 周正飞 on 17/5/9.
+//  Copyright © 2017年 周正飞. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let MAINCOLOR: UIColor = #colorLiteral(red: 0.2546484768, green: 0.2665711939, blue: 0.5550796986, alpha: 1)
